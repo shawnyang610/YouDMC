54da4cd08808b300188ef0eb4b2a6e6292d82df4
Jinqiu Liu <playdafuture@gmail.com>
2018-09-10 13:28:51 -0400

    using JS to add content test

--- Web/scripts/test.js
+++ Web/scripts/test.js
@@ -1,3 +1,9 @@
 function setup() {
-  createP("Test P")
+  document.getElementById("demo").innerHTML="JS running"; //indicate js is working
+}
+
+function insertTimeStamp() {  
+  var newP = document.createElement("p");
+  newP.appendChild(document.createTextNode(Date()));
+  document.getElementById("contents").appendChild(newP);
 }
