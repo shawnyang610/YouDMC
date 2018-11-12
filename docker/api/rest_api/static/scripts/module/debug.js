a8b75012fa2ec6448146191c80974d9b4b933d78
Shawn Yang <shawnyang610@gmail.com>
2018-11-12 14:35:06 -0500

    deployment for test

--- docker/api/rest_api/static/scripts/module/debug.js
+++ docker/api/rest_api/static/scripts/module/debug.js
@@ -1,25 +1,25 @@
 var debugDivID = "debug";
 
 function fillDebugButtons() {
-  var refreshButton = document.createElement("button");
-  refreshButton.appendChild(document.createTextNode("Refresh Messages"));
-  refreshButton.setAttribute("onclick","refreshMessages()");
-
-  var logTokenButton = document.createElement("button");
-  logTokenButton.appendChild(document.createTextNode("Log Auth Token"));
-  logTokenButton.setAttribute("onclick","logAuthToken()");
-
-  var pageInfo = document.createElement("p");
-  pageInfo.innerHTML = "Current Page = " + videoID;
-  var authInfo = document.createElement("p");
-  authInfo.innerHTML = "AuthToken = " + authToken;
-
-  var debugDiv = document.getElementById(debugDivID);
-  debugDiv.appendChild(refreshButton);
-  debugDiv.appendChild(logTokenButton);
-
-  debugDiv.appendChild(pageInfo);
-  debugDiv.appendChild(authInfo);
+  // var refreshButton = document.createElement("button");
+  // refreshButton.appendChild(document.createTextNode("Refresh Messages"));
+  // refreshButton.setAttribute("onclick","refreshMessages()");
+  //
+  // var logTokenButton = document.createElement("button");
+  // logTokenButton.appendChild(document.createTextNode("Log Auth Token"));
+  // logTokenButton.setAttribute("onclick","logAuthToken()");
+  //
+  // var pageInfo = document.createElement("p");
+  // pageInfo.innerHTML = "Current Page = " + videoID;
+  // var authInfo = document.createElement("p");
+  // authInfo.innerHTML = "AuthToken = " + authToken;
+  //
+  // var debugDiv = document.getElementById(debugDivID);
+  // debugDiv.appendChild(refreshButton);
+  // debugDiv.appendChild(logTokenButton);
+  //
+  // debugDiv.appendChild(pageInfo);
+  // debugDiv.appendChild(authInfo);
 }
 
 function refreshMessages() {
