f84ec139f9682f2760d26582580c98abdc88020b
stanostr <stan_ostr@yahoo.com>
2018-12-04 13:36:26 -0500

    Testing branch

--- YouDMCapp/app/src/main/java/com/youcmt/youdmcapp/LoginActivity.java
+++ YouDMCapp/app/src/main/java/com/youcmt/youdmcapp/LoginActivity.java
@@ -25,7 +25,6 @@ import static com.youcmt.youdmcapp.Constants.USER_ID;
  */
 public class LoginActivity extends AppCompatActivity implements LoginCallbacks {
     private static final String TAG = "LoginActivity";
-    private static final String TEST = "Branchtest";
     private SharedPreferences.Editor mEditor;
 
     @Override
